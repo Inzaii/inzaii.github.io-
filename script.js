090646c4417d0fa54200070c20284f81af8132d8
Inzaii <maxbeneda888@gmail.com>
2025-02-28 12:41:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        alert("Ваше повідомлення надіслано!");
+        form.reset();
+    });
 
+    const icons = document.querySelectorAll(".icon");
+    icons.forEach(icon => {
+        icon.addEventListener("mouseover", () => {
+            icon.style.color = "#ff9a9e";
+        });
+        icon.addEventListener("mouseout", () => {
+            icon.style.color = "#000";
+        });
+    });
+});
